bee6f6f6953dbeef6c5c16e38da138cf1aadb332
XrulDroidX <89652402+XrulDroidX@users.noreply.github.com>
2025-07-16 14:23:23 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+
+# Owner Configuration
+OWNER_ID = 7954328801
+OWNER_USERNAME = "@Alfyanda"
+
+# Paths
+QRIS_IMAGE_PATH = "assets/qris.jpg"
+PREMIUM_DB = "premium_users.txt"
+
+# Font paths
+FONT_ANDROID = "assets/fonts/roboto.ttf"
+FONT_IOS = "assets/fonts/sf-pro.ttf"
+
+# Environment Variable for Telegram Bot Token
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
